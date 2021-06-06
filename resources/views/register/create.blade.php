756856c7d89619ce234077e4824c97b0f380c02f
Darren Henderson <darrenh@ccsj.edu>
2021-06-06 07:40:58 -0500

    Finish video 47 Failed Validation Errors and Old Input.

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -28,11 +28,12 @@
             type="text"
             name="name"
             id="name"
+            value="{{ old('name') }}"
             required
         >
         
         @error('name')
-            <p class="text-red-500 text-xs mt-2"></p>
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
         @enderror
 
     </div>
@@ -48,11 +49,12 @@
             type="text"
             name="username"
             id="username"
+            value="{{ old('username') }}"
             required
         >
         
         @error('username')
-            <p class="text-red-500 text-xs mt-2"></p>
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
         @enderror
 
     </div>
@@ -68,11 +70,12 @@
                type="text"
                name="email"
                id="email"
+               value="{{ old('email') }}"
                required
         >
         
         @error('email')
-            <p class="text-red-500 text-xs mt-2"></p>
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
         @enderror
     
     </div>
@@ -92,7 +95,7 @@
         >
         
         @error('password')
-            <p class="text-red-500 text-xs mt-2"></p>
+            <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
         @enderror
 
     </div>
@@ -104,6 +107,14 @@
     Submit
     </button>
 </div>
+
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li class="text-red-500 text-xs">{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 
 </form>
 
